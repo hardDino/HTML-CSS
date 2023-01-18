c75c4f65bb121286e42588d759707f12a0960eec
Denis Pavlov <pavlov.github@gmail.com>
2023-01-18 18:47:12 +0400

    created services-inner page, added hero section, started offer section

--- createx/createx_src/src/js/components/slider.js
+++ createx/createx_src/src/js/components/slider.js
@@ -7,88 +7,92 @@ const gap = parseInt(bodyStyles.getPropertyValue('--grid-gap'));
 
 const portSlider = document.querySelector('.portfolio-section__items')
 
-const portfolioSlider = new Swiper(portSlider, {
-  slidesPerView: 3,
-  spaceBetween: gap,
-  loop: true,
-  centeredSlides: true,
-  on: {
-    init: function () {
+if (portSlider) {
+  const portfolioSlider = new Swiper(portSlider, {
+    slidesPerView: 3,
+    spaceBetween: gap,
+    loop: true,
+    centeredSlides: true,
+    on: {
+      init: function () {
 
-      const activeSlide = portSlider.querySelector('.swiper-slide-active');
+        const activeSlide = portSlider.querySelector('.swiper-slide-active');
 
-      const nextActiveSlide = activeSlide.nextElementSibling;
+        const nextActiveSlide = activeSlide.nextElementSibling;
 
-      const previousActiveSlide = activeSlide.previousElementSibling;
+        const previousActiveSlide = activeSlide.previousElementSibling;
 
-      activeSlide.classList.add('slider-visible');
-      nextActiveSlide.classList.add('slider-visible');
-      previousActiveSlide.classList.add('slider-visible');
+        activeSlide.classList.add('slider-visible');
+        nextActiveSlide.classList.add('slider-visible');
+        previousActiveSlide.classList.add('slider-visible');
+      }
+    },
+    navigation: {
+      nextEl: '.portfolio-section__next',
+      prevEl: '.portfolio-section__prev',
+    },
+    //ARIA
+    a11y: {
+      enabled: true,
+      prevSlideMessage: 'Previous slide',
+      nextSlideMessage: 'Next slide',
+      firstSlideMessage: 'This is the first slide',
+      lastSlideMessage: 'This is the last slide',
     }
-  },
-  navigation: {
-    nextEl: '.portfolio-section__next',
-    prevEl: '.portfolio-section__prev',
-  },
-  //ARIA
-  a11y: {
-    enabled: true,
-    prevSlideMessage: 'Previous slide',
-    nextSlideMessage: 'Next slide',
-    firstSlideMessage: 'This is the first slide',
-    lastSlideMessage: 'This is the last slide',
-  }
-});
+  });
 
-// document.querySelector('.portfolio-section__prev').addEventListener('click', () => {
+  // document.querySelector('.portfolio-section__prev').addEventListener('click', () => {
 
-//   const activeSlide = portSlider.querySelector('.swiper-slide-next');
+  //   const activeSlide = portSlider.querySelector('.swiper-slide-next');
 
-//   document.querySelectorAll('.portfolio-section__items .swiper-slide').forEach(el => {
-//     el.classList.remove('slider-visible');
-//   })
+  //   document.querySelectorAll('.portfolio-section__items .swiper-slide').forEach(el => {
+  //     el.classList.remove('slider-visible');
+  //   })
 
-//   if (activeSlide.previousElementSibling) {
-//     const nextActiveSlide = activeSlide.previousElementSibling;
-//     activeSlide.classList.add('slider-visible');
-//     nextActiveSlide.classList.add('slider-visible');
-//     previousSlide.nextElementSibling.classList.add('slider-visible');
-//   }
-// });
+  //   if (activeSlide.previousElementSibling) {
+  //     const nextActiveSlide = activeSlide.previousElementSibling;
+  //     activeSlide.classList.add('slider-visible');
+  //     nextActiveSlide.classList.add('slider-visible');
+  //     previousSlide.nextElementSibling.classList.add('slider-visible');
+  //   }
+  // });
 
-document.querySelector('.portfolio-section__next').addEventListener('click', () => {
+  document.querySelector('.portfolio-section__next').addEventListener('click', () => {
 
-  const activeSlide = portSlider.querySelector('.swiper-slide-active');
+    const activeSlide = portSlider.querySelector('.swiper-slide-active');
 
-  const nextActiveSlide = activeSlide.nextElementSibling;
+    const nextActiveSlide = activeSlide.nextElementSibling;
 
-  const previousActiveSlide = activeSlide.previousElementSibling;
+    const previousActiveSlide = activeSlide.previousElementSibling;
 
-  document.querySelectorAll('.portfolio-section__items .swiper-slide').forEach(el => {
-    el.classList.remove('slider-visible');
-  })
+    document.querySelectorAll('.portfolio-section__items .swiper-slide').forEach(el => {
+      el.classList.remove('slider-visible');
+    })
 
-  activeSlide.classList.add('slider-visible');
-  nextActiveSlide.classList.add('slider-visible');
-  previousActiveSlide.classList.add('slider-visible');
-});
+    activeSlide.classList.add('slider-visible');
+    nextActiveSlide.classList.add('slider-visible');
+    previousActiveSlide.classList.add('slider-visible');
+  });
 
-document.querySelector('.portfolio-section__prev').addEventListener('click', () => {
+  document.querySelector('.portfolio-section__prev').addEventListener('click', () => {
 
-  const activeSlide = portSlider.querySelector('.swiper-slide-active');
+    const activeSlide = portSlider.querySelector('.swiper-slide-active');
 
-  const nextActiveSlide = activeSlide.previousElementSibling;
+    const nextActiveSlide = activeSlide.previousElementSibling;
 
-  const previousActiveSlide = activeSlide.nextElementSibling;
+    const previousActiveSlide = activeSlide.nextElementSibling;
 
-  document.querySelectorAll('.portfolio-section__items .swiper-slide').forEach(el => {
-    el.classList.remove('slider-visible');
-  })
+    document.querySelectorAll('.portfolio-section__items .swiper-slide').forEach(el => {
+      el.classList.remove('slider-visible');
+    })
 
-  activeSlide.classList.add('slider-visible');
-  nextActiveSlide.classList.add('slider-visible');
-  previousActiveSlide.classList.add('slider-visible');
-});
+    activeSlide.classList.add('slider-visible');
+    nextActiveSlide.classList.add('slider-visible');
+    previousActiveSlide.classList.add('slider-visible');
+  });
+}
+
+
 
 const testimonialsSlider = new Swiper('.testimonials__items', {
   slidesPerView: 1,
