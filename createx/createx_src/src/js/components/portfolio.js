606eadc5e0bfbe74cfc08c5e11e6c910c899b3b7
Denis Pavlov <pavlov.github@gmail.com>
2023-01-24 14:51:24 +0400

    finished js code

--- createx/createx_src/src/js/components/portfolio.js
+++ createx/createx_src/src/js/components/portfolio.js
@@ -1,6 +1,16 @@
 const portfolioTabsNav = document.querySelector('.portfolio-tabs-nav');
 const portfolioTabsBtns = document.querySelectorAll('.portfolio-tabs-nav__btn');
 const portfolioTabsItems = document.querySelectorAll('.portfolio-tabs__item');
+const portfolioTabsItemsVisible = document.querySelectorAll('.portfolio-tabs__item--visible');
+const loadMore = document.querySelector('.portfolio-load');
+
+const isLoadMoreNeeded = (selector) => {
+  if (selector.length <= 9) {
+    loadMore.style.display = 'none';
+  } else {
+    loadMore.style.display = 'inline-flex';
+  }
+}
 
 portfolioTabsNav.addEventListener('click', (e) => {
   const target = e.target;
@@ -15,19 +25,43 @@ portfolioTabsNav.addEventListener('click', (e) => {
     target.classList.add('portfolio-tabs-nav__btn--active');
 
     portfolioTabsItems.forEach(el => {
-      el.style.display = 'none'
       el.classList.remove('portfolio-tabs__item--visible')
+      el.classList.remove('portfolio-tabs__item--visible-more')
     });
 
     document.querySelectorAll(`[data-target="${path}"]`).forEach(el => {
       el.closest('.portfolio-tabs__item').classList.add('portfolio-tabs__item--visible')
     });
 
+    isLoadMoreNeeded(document.querySelectorAll(`[data-target="${path}"]`));
+
     if ( path == 'all') {
+
       portfolioTabsItems.forEach(el => {
-        el.style.display = 'block'
-        //el.classList.remove('portfolio-tabs__item--visible')
+        el.classList.add('portfolio-tabs__item--visible')
       });
+
+      isLoadMoreNeeded(document.querySelectorAll('.portfolio-tabs__item--visible'));
     }
   }
 })
+
+isLoadMoreNeeded(portfolioTabsItemsVisible);
+
+loadMore.addEventListener('click', (e) => {
+  //const visibleItems = document.querySelectorAll('.portfolio-tabs__item--visible');
+
+  const path = document.querySelector('.portfolio-tabs-nav__btn--active').dataset.path;
+
+  if(path == 'all') {
+    portfolioTabsItems.forEach(el => {
+      el.classList.add('portfolio-tabs__item--visible-more');
+      loadMore.style.display = 'none';
+    });
+  } else {
+    document.querySelectorAll(`[data-target="${path}"]`).forEach(el => {
+      el.closest('.portfolio-tabs__item').classList.add('portfolio-tabs__item--visible-more');
+    });
+    loadMore.style.display = 'none';
+  }
+});
